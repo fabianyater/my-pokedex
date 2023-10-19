e71b36b742a0c6a8d676e3a007e5ae2aa1b86f28
Fabian Yate <fabianyate31@gmail.com>
2023-10-19 18:58:03 -0500

    Add favicon

--- src/img/images.js
+++ src/img/images.js
@@ -1,9 +1,3 @@
-import fire from './fire.png'
-import bulbasaur from './bulbasaur.png'
-import charizard from './charizard.png'
-import mewtwo from './mewtwo.png'
-import pikachu from './pikachu.png'
-import squirtle from './squirtle.png'
 import pokeball from './pokeball.png'
 
-export { fire, pokeball, bulbasaur, charizard, mewtwo, pikachu, squirtle }
+export { pokeball }
